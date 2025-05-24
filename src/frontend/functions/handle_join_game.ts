3f9af347f1764ed9e4dc59633615fd9fdb4d21d7
Matija Bogdanovic <matijabog45@gmail.com>
2025-05-24 16:12:28 +0200

    Login https request fix

--- src/frontend/functions/handle_join_game.ts
+++ src/frontend/functions/handle_join_game.ts
@@ -13,7 +13,7 @@ export async function handleJoinGame() {
     userCountEl.innerText = "Username mustn't be shorter than 3 letters";
     return;
   }
-  await httpFunction("https://whoisfaster.onrender.com/", {
+  await httpFunction("https://whoisfaster.onrender.com/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
